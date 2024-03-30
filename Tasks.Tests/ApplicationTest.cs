abbb9da61f52ff1f2896630cceaa4ce48bf43da5
SIAOYUCHEN <siaoyuchen@icsc.com.tw>
2024-03-30 13:52:55 +0800

    [feat][Add New TestCase Run_Quits_WhenQuitCommandIsEntered]

--- Tasks.Tests/ApplicationTest.cs
+++ Tasks.Tests/ApplicationTest.cs
@@ -1,111 +1,39 @@
-// using System;
-// using System.Threading;
-// using NUnit.Framework;
-//
-// namespace Tasks
-// {
-// 	[TestFixture]
-// 	public sealed class ApplicationTest
-// 	{
-// 		public const string PROMPT = "> ";
-//
-// 		private FakeConsole console;
-// 		private Thread applicationThread;
-//
-// 		[SetUp]
-// 		public void StartTheApplication()
-// 		{
-// 			this.console = new FakeConsole();
-// 			var taskList = new TaskList(console);
-// 			this.applicationThread = new Thread(() => taskList.Run());
-// 			applicationThread.Start();
-// 		}
-//
-// 		[TearDown]
-// 		public void KillTheApplication()
-// 		{
-// 			if (applicationThread == null || !applicationThread.IsAlive)
-// 			{
-// 				return;
-// 			}
-//
-// 			applicationThread.Abort();
-// 			throw new Exception("The application is still running.");
-// 		}
-//
-// 		[Test, Timeout(1000)]
-// 		public void ItWorks()
-// 		{
-// 			Execute("show");
-//
-// 			Execute("add project secrets");
-// 			Execute("add task secrets Eat more donuts.");
-// 			Execute("add task secrets Destroy all humans.");
-//
-// 			Execute("show");
-// 			ReadLines(
-// 				"secrets",
-// 				"    [ ] 1: Eat more donuts.",
-// 				"    [ ] 2: Destroy all humans.",
-// 				""
-// 			);
-//
-// 			Execute("add project training");
-// 			Execute("add task training Four Elements of Simple Design");
-// 			Execute("add task training SOLID");
-// 			Execute("add task training Coupling and Cohesion");
-// 			Execute("add task training Primitive Obsession");
-// 			Execute("add task training Outside-In TDD");
-// 			Execute("add task training Interaction-Driven Design");
-//
-// 			Execute("check 1");
-// 			Execute("check 3");
-// 			Execute("check 5");
-// 			Execute("check 6");
-//
-// 			Execute("show");
-// 			ReadLines(
-// 				"secrets",
-// 				"    [x] 1: Eat more donuts.",
-// 				"    [ ] 2: Destroy all humans.",
-// 				"",
-// 				"training",
-// 				"    [x] 3: Four Elements of Simple Design",
-// 				"    [ ] 4: SOLID",
-// 				"    [x] 5: Coupling and Cohesion",
-// 				"    [x] 6: Primitive Obsession",
-// 				"    [ ] 7: Outside-In TDD",
-// 				"    [ ] 8: Interaction-Driven Design",
-// 				""
-// 			);
-//
-// 			Execute("quit");
-// 		}
-//
-// 		private void Execute(string command)
-// 		{
-// 			Read(PROMPT);
-// 			Write(command);
-// 		}
-//
-// 		private void Read(string expectedOutput)
-// 		{
-// 			var length = expectedOutput.Length;
-// 			var actualOutput = console.RetrieveOutput(expectedOutput.Length);
-// 			Assert.AreEqual(expectedOutput, actualOutput);
-// 		}
-//
-// 		private void ReadLines(params string[] expectedOutput)
-// 		{
-// 			foreach (var line in expectedOutput)
-// 			{
-// 				Read(line + Environment.NewLine);
-// 			}
-// 		}
-//
-// 		private void Write(string input)
-// 		{
-// 			console.SendInput(input + Environment.NewLine);
-// 		}
-// 	}
-// }
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using Tasks.Application.UserInterface;
+using Tasks.Domain.Entity;
+using Tasks.Infrastructure;
+
+namespace Tasks
+{
+	[TestFixture]
+	public sealed class ApplicationTest
+	{
+		private IConsole _console;
+		private CommandFactory _commandFactory;
+		private TaskList _taskList;
+		private ITaskRepository _taskRepository;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_console = Substitute.For<IConsole>();
+			_taskRepository = Substitute.For<ITaskRepository>();
+			_commandFactory = new CommandFactory(_taskRepository, _console);
+			_taskList = new TaskList(_console, _commandFactory);
+		}
+
+		[Test]
+		public void Run_Quits_WhenQuitCommandIsEntered()
+		{
+			_console.ReadLine().Returns("quit");
+
+			_taskList.Run();
+
+			_console.Received(1).Write("> ");
+		}
+
+		
+	}
+}
